cd43c2b7d05a5827e5b1b9936f7f26fbf74dfa8b
PoChuan994 <pochuan994@gmail.com>
2025-03-10 14:11:28 +0800

    Create 'insert_head', 'create' and modify 'insert_tail'

--- dlist.c
+++ dlist.c
@@ -6,19 +6,39 @@ typedef struct node {
   struct node *next, *prev;
 } node;
 
-void insert_tail(struct node **start, int value) {
-  if (!*start) {
+void create(node **start, int value)
+{
     node *new = malloc(sizeof(node));
     new->data = value;
-    new->next = new_node->prev = new_node;
-    *start = new_node;
+    new->next = new->prev = new;
+    *start = new;
     return;
-  }
-  struct node *last = (*start)->prev;
-  struct node *new = malloc(sizeof(node));
-  new->data = value;
-  new->next = *start;
-  new->prev = last;
-  (*start)->prev = new;
-  last->next = new;
+}
+
+void insert_tail(node **start, int value)
+{
+    if (!*start)
+        return create(start, value);
+
+    struct node *last = (*start)->prev;
+    struct node *new = malloc(sizeof(node));
+    new->data = value;
+    new->next = *start;
+    new->prev = last;
+    (*start)->prev = new;
+    last->next = new;
+}
+
+void insert_head(node **start, int value)
+{
+    if(!start)
+        return create(start, value);
+
+    node last = (*start)->prev;
+    node new = malloc(sizeof(node));
+    node->data = value;
+    node->next = *start;
+    node->prev = last;
+    (*start)->prev = last->next = new;
+    *start = new;
 }
